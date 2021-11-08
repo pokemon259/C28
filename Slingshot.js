d14c49fd4d316498c28a709e6304063f792d857e
pokemon259 <76998840+pokemon259@users.noreply.github.com>
2021-11-08 12:48:27 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+        this.pointB=pointB;
+    }
+
+    fly(){
+        this.sling.bodyA=null;
+    }
+
+
+    
+
+    display(){
+        if (this.sling.bodyA) {
+        
+        var posA = this.sling.bodyA.position;
+        var posB = this.pointB;
+        strokeWeight(4);
+        line(posA.x, posA.y, posB.x, posB.y);
+    }
+}
+}
